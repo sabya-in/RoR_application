5077f49c324c437958422e6822c30fdafc5aa892
sabya-in <sachi.iiest@gmail.com>
2022-03-09 10:31:19 +0530

    static_pages + TDD start

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    @title = "Home"
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  def help
+    @title = "Help"
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+end
