eb7adfdb0321a5f11b6fd959dc2f11f01f2b1c70
sabya-in <sachi.iiest@gmail.com>
2022-03-11 18:50:41 +0530

    user activerecord : validation in email password

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development do
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.2'
   gem 'bootstrap-sass', '3.4.1'
+  gem 'bcrypt', '~> 3.1.7'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    before_save { self.email = email.downcase }
     has_many :micropost
+    validates :name, presence: true, length: {maximum: 50} 
+    validates :email, presence: true, length: {maximum: 255}, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
+    validates :password, presence: true, length: { minimum: 6 }
+    has_secure_password
 end
